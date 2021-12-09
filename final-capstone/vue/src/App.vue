8e447fd23af09931ee9260e2e3f79f0dc73f77b7
Robert Kaiser <kaiserrj@gmail.com>
2021-12-09 16:39:23 -0500

    added vuetify

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -1,9 +1,37 @@
+
 <template>
-  <div id="app">
-    <div id="nav">
+  <v-app>
+    <!-- Must have the app property -->
+    <v-app-bar app>
+      <div class="logo">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://pupuphooray.com/wp-content/uploads/2019/03/dog-icon.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+      <div class="title">TEST</div>
+      <v-spacer></v-spacer>
+      <router-link v-bind:to="{ name: 'home' }">
+        <v-btn rounded color="primary" dark> Home </v-btn>
+      </router-link>
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"><v-btn rounded color="primary" dark> logout </v-btn></router-link>
+      
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+<!--
+<div id="app">
+<div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
-  </div>
-</template>
+       </div> -->
