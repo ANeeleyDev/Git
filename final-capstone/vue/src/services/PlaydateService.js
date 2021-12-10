b8c0b51692aa188e250381be3b767e04bfc65f0d
Amanda Neeley <mgrasa1@gmail.com>
2021-12-10 11:26:48 -0500

    playdate services done

--- final-capstone/vue/src/services/PlaydateService.js
+++ final-capstone/vue/src/services/PlaydateService.js
@@ -5,15 +5,34 @@ export default {
     //get all playdates
 
     playdateList() {
-        return axios.get('/playdate');
+        return axios.get(`/playdates`);
+    },
+        // return logged in users list of playdates
+    
+    getPlaydatesByUserId(userId){
+        return axios.get(`/playdates/user/${userId}`)
+    },
 
-        // return specific playdate details
+        // return specific playdate details by playdate ID
+
+    getPlaydateByPlaydateId(playdateId){
+        return axios.get(`/playdates/${playdateId}`)
+    },
 
         //creates new playdate
+    registerPlaydate(playdateToSave){
+        return axios.post(`/playdates/myplaydates/register`, playdateToSave)
+    },
 
         //edit specific playdate
 
+    updateLoggedInUserPlaydate(updatedPlaydate, playdateId){
+        return axios.put(`/playdates/myplaydates/${playdateId}`,updatedPlaydate)
+    },
+
         //delete playdate
-    }
+    deleteLoggedInUserPlaydate(playdateId){
+        return axios.delete(`/myplaydates/${playdateId}`)
+    },
 
 }
